8ac42a5a8e3be80491fabc9f2bf9195b2f6cc0e9
宇 李 <xiaoyutiger@126.com>
2019-03-25 19:29:32 +0800

    need output

--- randomizedSelect/randomizedSelect/main.cpp
+++ randomizedSelect/randomizedSelect/main.cpp
@@ -1,55 +1,77 @@
 #include "HEAD.h"
 
-int main()
+int main()		//随机生成大小为N的数组，输出用插入排序、9.2算法和9.3算法求得的第INIT小的数，以及算法执行的时间
 {
-	int A[N], B[N];
-	int p = 0;
-	int r = N - 1;
-	int i = INIT;
+	int A[N], B[N];			//数组B用来记录原数组，数组A在函数中被改变
+	int p = 0;				//起始下标
+	int r = N - 1;			//结束下标
+	int i = INIT;			
 
-	time_t beginT, endT;
-	time_t during1, during2;
+	time_t beginT, endT;	//开始时间、结束时间
+	time_t during0, during1, during2;//插入排序运行时间、9.2算法运行时间、9.3算法运行时间
 	
-	//srand(457);
+	
+
 	srand((unsigned)time(NULL));
-
-	for (int i = 0; i < N; i++)
-		B[i] = rand();	    //B初始化
-
-	//-----------------------------------------
-
-
-	for (int i = 0; i < N; i++)		//A初始化
-		A[i] = B[i];
-
-	beginT = clock();
-	int result1 = randomizedSelect(A, p, r, i);
-	endT = clock();
-	during1 = endT - beginT;		//9.2
-
-	cout << "result1:" << endl;
-	for (int i = 0; i < N; i++)
-		cout << A[i] << endl;
-	cout << endl << endl;
-	//---------------------------------------------
-
+	//srand(14);
 	
-	for (int i = 0; i < N; i++)		//A初始化
-		A[i] = B[i];
+	for (int j = 1; j <= 10;j++)
+	{
 
-	beginT = clock();
-	int result2 = worstCaseSelect(A);
-	endT = clock();
-	during2 = endT - beginT;		//9.3
 
-	cout << "result2" << endl;
-	for (int i = 0; i < N; i++)
-		cout << A[i] << endl;
-	
-	//---------------------------------------------
-	
+		for (int i = 0; i < N; i++)
+			B[i] = rand();	    //B初始化
 
-		
+		//-----------------------------------------
+
+		/*插入排序用来测试结果是否正确
+		for (int i = 0; i < N; i++)		//A初始化
+			A[i] = B[i];
+
+		beginT = clock();
+		insertionSort(A, 0, N - 1);
+		endT = clock();					//记录算法开始和结束的时间
+		during0 = endT - beginT;
+		cout << "result of insertion sort:" << endl;
+		cout << A[INIT - 1] << endl;	//插入排序，输出第i小的数
+		cout << during0;				//输出算法执行的时间
+		cout << endl << endl;
+		*/
+		//-----------------------------------------
+
+		for (int i = 0; i < N; i++)		//A初始化
+			A[i] = B[i];
+
+		beginT = clock();
+		int result1 = randomizedSelect(A, p, r, i);//执行9.2算法得到第i小的数
+		endT = clock();					//记录算法开始和结束的时间
+		during1 = endT - beginT;
+
+		cout << "result1:";
+		cout << result1 << endl;
+		cout << during1;
+		cout << endl << endl;
+
+		//---------------------------------------------
+
+
+		for (int i = 0; i < N; i++)		//A初始化
+			A[i] = B[i];
+
+		beginT = clock();
+		int result2 = worstCaseSelect(A);//执行9.3算法得到第i小的数
+		endT = clock();					//记录算法开始和结束的时间
+		during2 = endT - beginT;
+
+		cout << "result2:";
+		cout << result2 << endl;
+		cout << during2;
+		cout << endl << endl;
+
+
+		//---------------------------------------------
+
+	};
 	getchar();
 	return 0;
 }
--- randomizedSelect/randomizedSelect/randomizedSelect.cpp
+++ randomizedSelect/randomizedSelect/randomizedSelect.cpp
@@ -5,10 +5,12 @@ int randomizedSelect(int A[], int p, int r, int i)
 	if (p == r)
 		return A[p];
 	int x = A[p];
-	int q = randomizedPartition(A, p, r);
-	int k = q - p + 1;
-	if (i == k+1)
+	int q = randomizedPartition(A, p, r);				//把A数组中下标在p到r之间的数分为<=A[p]和>=A[p]的部分，返回<=A[p]部分的最大下标
+	int k = q - p + 1;									//x是数组A中第k小的数
+	if (i == k+1)										//如果x是数组A中第i小的数
 		return(x);
+	if (k == 0)											//这时A[q+1]是目前范围内中最小的数，应该从A[q+2]开始找第i-1小的数
+		return(randomizedSelect(A, q + 2, r, i - 1));
 	else if (i < k+1)
 		return(randomizedSelect(A, p, q, i));
 	else
@@ -20,7 +22,7 @@ int randomizedPartition(int A[], int p, int r)
 	int x = A[p];
 	int i = p - 1;
 	int j = r + 1;
-	int flag = 0;
+	int flag = 0;			//flag记录有没有进行交换，如果没有交换，说明A[p]是最小的数。有交换的情况下，x是第j+2小的数，但没有交换时x是第j+1小的数，返回特殊值p-1用来区分。
 	while (1)
 	{
 		do
@@ -40,7 +42,10 @@ int randomizedPartition(int A[], int p, int r)
 		}
 		else
 		{
-				return j;
+			if (flag == 1)
+				return(j);
+			else
+				return(j - 1);
 		}
 	}
 }
