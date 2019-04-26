d3f689b59d496789c003150eff5b0c34f71b64db
Alex Zhou <alexzhoucs@gmail.com>
2019-04-26 15:17:54 +0800

    completed

--- numbersReformat/numbersReformat/numbersReformat.cpp
+++ numbersReformat/numbersReformat/numbersReformat.cpp
@@ -1,19 +1,39 @@
-﻿
+﻿//输入txt文件，共100k个数据，改写成1k行，每行100个数据的csv文件
 
 #include <iostream>
+#include <fstream>
+
+#define INPUTFILE		"./result.txt"
+#define OUTPUTFILE		"./result.csv"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    //std::cout << "Hello World!\n"; 
+	int data[100];
+	ifstream in;
+	in.open(INPUTFILE, ios::binary);
+	ofstream out;
+	out.open(OUTPUTFILE, ios::binary);
+
+	int lines = 0;
+
+	while (!in.eof())
+	{
+		for (int times = 0; times < 100; times++)
+		{
+			int data;
+			in >> data;
+			out << data << ',';
+		}
+		out << "\n";
+		lines++;
+	}
+
+	cout << "number of lines: " << lines << endl;
+	
+	in.close();
+	out.close();
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
